fd4d89751b7010fef68ac5f8f0dec8697ede0e4d
梁智敏 <lgemon@hotmail.com>
2011-08-17 01:48:20 +0800

    A more interesting example.

--- codes/use_ruby.scala
+++ codes/use_ruby.scala
@@ -9,13 +9,44 @@ val keys = h.getClass().getMethod("keys").invoke(h)
 println(keys.getClass().getMethod("size").invoke(keys))
 
 val script = """
-class FunWithJRuby
-  def self.double(n)
-    n * 2
+require 'date'
+
+class Person
+  attr_accessor :name, :birthday, :energy
+
+  def initialize(attributes = nil)
+    attributes.each { |k, v| send "#{k}=", v } if attributes
+    self.energy ||= 0
+  end
+
+  def say_hi
+    "Hello, my name is #{name}"
+  end
+
+  def age
+    Date.today.year - birthday.year
+  end
+
+  def hungry?
+    energy < 50
+  end
+
+  def dead?
+    energe <= 10
+  end
+
+  def work
+    energy -= 60 unless dead?
+  end
+
+  def eat
+    energy += 30 unless dead?
   end
 end
-
-FunWithJRuby.double 100
 """
 
-println(context.parse(script).run())
+val receiver = context.parse(script).run()
+val attrs = new java.util.HashMap[String, String](1, 1)
+attrs.put("name", "Gimi")
+val gimi = context.callMethod(context.get(receiver, "Person"), "new", attrs)
+println(context.callMethod(gimi, "say_hi"))
